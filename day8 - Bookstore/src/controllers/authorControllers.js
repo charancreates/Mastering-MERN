a115f3250152892f92a3c092640618be0366c4ea
Charan <charan.r.tech@gmail.com>
2026-05-22 23:51:32 +0530

    mongoose  mastered -charan

--- day8 - Bookstore/src/controllers/authorControllers.js
+++ day8 - Bookstore/src/controllers/authorControllers.js
@@ -23,7 +23,32 @@ export const updateAuthor = async (req, res, next) => {
       returnDocument: "after",
       runValidators: true,
     });
+    if (!author) {
+      return next(createError(404, "Author does not exist"));
+    }
     res.json(author);
+  } catch (error) {
+    next(error);
+  }
+};
+export const getAuthor = async (req, res, next) => {
+  try {
+    const author = await Author.findById(req.params.id);
+    if (!author) {
+      return next(createError(404, "Author does not exist"));
+    }
+    res.json(author);
+  } catch (error) {
+    next(error);
+  }
+};
+export const deleteAuthor = async (req, res, next) => {
+  try {
+    const author = await Author.findByIdAndDelete(req.params.id);
+    if (!author) {
+      return next(createError(404, "Author does not exist"));
+    }
+    res.json("author deleted successffully");
   } catch (error) {
     next(error);
   }
--- day8 - Bookstore/src/routes/authorRoutes.js
+++ day8 - Bookstore/src/routes/authorRoutes.js
@@ -1,8 +1,19 @@
 import Router from "express";
-import { createAuthor, getAuthors } from "../controllers/authorControllers.js";
+import {
+  createAuthor,
+  deleteAuthor,
+  getAuthor,
+  getAuthors,
+  updateAuthor,
+} from "../controllers/authorControllers.js";
 
 const authorRoutes = Router();
 
 authorRoutes.route("/").get(getAuthors).post(createAuthor);
+authorRoutes
+  .route("/:id")
+  .put(updateAuthor)
+  .get(getAuthor)
+  .delete(deleteAuthor);
 
 export default authorRoutes;
