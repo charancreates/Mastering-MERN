63586b8feb3e98e1487713ef5b30e1715c320ced
Charan <charan.r.tech@gmail.com>
2026-05-26 18:51:10 +0530

    added bycrypt and jwt -charan

--- Notes app/backend/src/controllers/userControllers.js
+++ Notes app/backend/src/controllers/userControllers.js
@@ -1,19 +1,27 @@
 import { User } from "../models/userschema.js";
 import createError from "http-errors";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
-export const getUsers = async (req, res, next) => {
+export const createUsers = async (req, res, next) => {
   try {
-    const users = await User.find();
-    res.json(users);
+    const { password, name, email } = req.body;
+    const hashedpass = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashedpass });
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "7d",
+    });
+    res.json({ name: name, email: email, token });
   } catch (error) {
     next(error);
   }
 };
 
-export const createUsers = async (req, res, next) => {
+export const getUsers = async (req, res, next) => {
   try {
-    const user = await User.create(req.body);
-    res.json(user);
+    const users = await User.find();
+    res.json(users);
   } catch (error) {
     next(error);
   }
