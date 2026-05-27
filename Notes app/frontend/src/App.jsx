5c1ec979b765bc8d9fdefcf5ef77e0b65d7e87d0
Charan <charan.r.tech@gmail.com>
2026-05-27 23:55:42 +0530

    MERN STACK MASTERED -CHARAN

--- Notes app/frontend/src/App.jsx
+++ Notes app/frontend/src/App.jsx
@@ -6,6 +6,7 @@ import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Login from "./Components/Login";
 import Notes from "./Components/Notes";
+import ProtectedRoute from "./Components/ProtectedRoute";
 
 function App() {
   return (
@@ -14,7 +15,14 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/notes" element={<Notes />} />
+        <Route
+          path="/notes"
+          element={
+            <ProtectedRoute>
+              <Notes />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </>
   );
